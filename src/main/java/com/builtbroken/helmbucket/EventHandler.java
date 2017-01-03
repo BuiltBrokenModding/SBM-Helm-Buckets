f52cb8c82e823229f979210b83308baf0cc04e78
Robert Seifert <rseifert.phone@gmail.com>
2017-01-03 03:55:52 -0500

    Mod is functional

--- src/main/java/com/builtbroken/helmbucket/EventHandler.java
+++ src/main/java/com/builtbroken/helmbucket/EventHandler.java
@@ -1,7 +1,8 @@
 package com.builtbroken.helmbucket;
 
-import cpw.mods.fml.common.eventhandler.Event;
+import com.builtbroken.helmbucket.network.PacketBucketAction;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
@@ -9,6 +10,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 /**
@@ -20,60 +22,70 @@ public class EventHandler
     @SubscribeEvent
     public void onPlayerRightClick(PlayerInteractEvent event)
     {
-        if (!event.world.isRemote)
+        if (pickupFluid(event.world, event.entityPlayer, false))
         {
-            ItemStack heldItem = event.entityPlayer.getHeldItem();
-            if (heldItem != null && heldItem.getItem() instanceof ItemArmor)
+            HelmBucket.packetHandler.sendToServer(new PacketBucketAction());
+            event.setCanceled(true);
+        }
+    }
+
+    public static boolean pickupFluid(World world, EntityPlayer entityPlayer, boolean doAction)
+    {
+        ItemStack heldItem = entityPlayer.getHeldItem();
+        if (heldItem != null && heldItem.getItem() instanceof ItemArmor)
+        {
+            Item item = heldItem.getItem();
+            if (item == Items.iron_helmet || item == Items.golden_helmet || item == Items.diamond_helmet)
             {
-                Item item = heldItem.getItem();
-                if (item == Items.iron_helmet || item == Items.golden_helmet || item == Items.diamond_helmet)
+                MovingObjectPosition movingobjectposition = HelmBucket.itemHelmBucket.getMovingObjectPositionFromPlayer(world, entityPlayer, true);
+                if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
                 {
-                    MovingObjectPosition movingobjectposition = HelmBucket.itemHelmBucket.getMovingObjectPositionFromPlayer(event.world, event.entityPlayer, true);
-                    if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
+                    int i = movingobjectposition.blockX;
+                    int j = movingobjectposition.blockY;
+                    int k = movingobjectposition.blockZ;
+
+                    if (!world.canMineBlock(entityPlayer, i, j, k))
                     {
-                        int i = movingobjectposition.blockX;
-                        int j = movingobjectposition.blockY;
-                        int k = movingobjectposition.blockZ;
+                        return false;
+                    }
 
-                        if (!event.world.canMineBlock(event.entityPlayer, i, j, k))
+                    if (!entityPlayer.canPlayerEdit(i, j, k, movingobjectposition.sideHit, heldItem))
+                    {
+                        return false;
+                    }
+
+                    Block block = world.getBlock(i, j, k);
+                    Material material = block.getMaterial();
+                    int l = world.getBlockMetadata(i, j, k);
+
+                    if (material == Material.water && l == 0)
+                    {
+                        if (doAction)
                         {
-                            event.useItem = Event.Result.ALLOW;
-                            return;
+                            world.setBlockToAir(i, j, k);
+                            entityPlayer.inventory.setInventorySlotContents(entityPlayer.inventory.currentItem, addToInventory(heldItem, entityPlayer, ItemHelmBucket.getStack(item, block)));
+                            entityPlayer.inventoryContainer.detectAndSendChanges();
                         }
+                        return true;
+                    }
 
-                        if (!event.entityPlayer.canPlayerEdit(i, j, k, movingobjectposition.sideHit, heldItem))
+                    if (material == Material.lava && l == 0 && item == Items.diamond_helmet)
+                    {
+                        if (doAction)
                         {
-                            event.useItem = Event.Result.ALLOW;
-                            return;
+                            world.setBlockToAir(i, j, k);
+                            entityPlayer.inventory.setInventorySlotContents(entityPlayer.inventory.currentItem, addToInventory(heldItem, entityPlayer, ItemHelmBucket.getStack(item, block)));
+                            entityPlayer.inventoryContainer.detectAndSendChanges();
                         }
-
-                        Material material = event.world.getBlock(i, j, k).getMaterial();
-                        int l = event.world.getBlockMetadata(i, j, k);
-
-                        if (material == Material.water && l == 0)
-                        {
-                            event.world.setBlockToAir(i, j, k);
-                            event.entityPlayer.inventory.setInventorySlotContents(event.entityPlayer.inventory.currentItem, this.addToInventory(heldItem, event.entityPlayer, ItemHelmBucket.getStack(item, material)));
-                            event.entityPlayer.inventoryContainer.detectAndSendChanges();
-                            event.useItem = Event.Result.ALLOW;
-                            return;
-                        }
-
-                        if (material == Material.lava && l == 0 && item == Items.diamond_helmet)
-                        {
-                            event.world.setBlockToAir(i, j, k);
-                            event.entityPlayer.inventory.setInventorySlotContents(event.entityPlayer.inventory.currentItem, this.addToInventory(heldItem, event.entityPlayer, ItemHelmBucket.getStack(item, material)));
-                            event.entityPlayer.inventoryContainer.detectAndSendChanges();
-                            event.useItem = Event.Result.ALLOW;
-                            return;
-                        }
+                        return true;
                     }
                 }
             }
         }
+        return false;
     }
 
-    private ItemStack addToInventory(ItemStack originalStack, EntityPlayer player, ItemStack newStack)
+    private static ItemStack addToInventory(ItemStack originalStack, EntityPlayer player, ItemStack newStack)
     {
         if (player.capabilities.isCreativeMode)
         {
--- src/main/java/com/builtbroken/helmbucket/ItemHelmBucket.java
+++ src/main/java/com/builtbroken/helmbucket/ItemHelmBucket.java
@@ -1,11 +1,22 @@
 package com.builtbroken.helmbucket;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
@@ -13,6 +24,12 @@ import net.minecraft.world.World;
  */
 public class ItemHelmBucket extends Item
 {
+    @SideOnly(Side.CLIENT)
+    IIcon[] fluidIcons;
+
+    @SideOnly(Side.CLIENT)
+    IIcon[] helmIcons;
+
     public ItemHelmBucket()
     {
         setUnlocalizedName("helmbucket:helmBucket");
@@ -20,9 +37,85 @@ public class ItemHelmBucket extends Item
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean b)
+    {
+        int meta = stack.getItemDamage();
+        int fluidMeta = meta % 1000;
+
+        if (fluidMeta == 1)
+        {
+            list.add(StatCollector.translateToLocal(Blocks.water.getUnlocalizedName()));
+        }
+        else if (fluidMeta == 2)
+        {
+            list.add(StatCollector.translateToLocal(Blocks.lava.getUnlocalizedName()));
+        }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIconFromDamageForRenderPass(int meta, int pass)
+    {
+        if (pass == 0)
+        {
+            return fluidIcons[meta % 1000];
+        }
+        return helmIcons[meta / 1000];
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIconFromDamage(int meta)
+    {
+        return helmIcons[meta / 1000];
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public boolean requiresMultipleRenderPasses()
+    {
+        return true;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister reg)
+    {
+        this.helmIcons = new IIcon[3];
+        this.helmIcons[0] = reg.registerIcon("helmbucket:iron_helmet");
+        this.helmIcons[1] = reg.registerIcon("helmbucket:gold_helmet");
+        this.helmIcons[2] = reg.registerIcon("helmbucket:diamond_helmet");
+
+        this.fluidIcons = new IIcon[3];
+        this.fluidIcons[0] = reg.registerIcon("helmbucket:blank");
+        this.fluidIcons[1] = reg.registerIcon("helmbucket:water");
+        this.fluidIcons[2] = reg.registerIcon("helmbucket:lava");
+    }
+
+    @Override
+    public void onUpdate(ItemStack stack, World world, Entity entity, int slot, boolean p_77663_5_)
+    {
+        //TODO make metal helms burn the player randomly
+    }
+
+    @Override
     public String getUnlocalizedName(ItemStack stack)
     {
-        return getUnlocalizedName() + "." + stack.getItemDamage();
+        int meta = stack.getItemDamage();
+        if (meta < 1000)
+        {
+            return getUnlocalizedName() + ".iron";
+        }
+        else if (meta < 2000)
+        {
+            return getUnlocalizedName() + ".gold";
+        }
+        if (meta < 3000)
+        {
+            return getUnlocalizedName() + ".diamond";
+        }
+        return getUnlocalizedName();
     }
 
     @Override
@@ -31,8 +124,162 @@ public class ItemHelmBucket extends Item
         return super.getMovingObjectPositionFromPlayer(world, player, flag);
     }
 
-    public static ItemStack getStack(Item item, Material material)
+    public static ItemStack getStack(Item item, Block block)
     {
-        return null;
+        int meta = 0;
+        if (item == Items.iron_helmet)
+        {
+            meta = 0;
+        }
+        else if (item == Items.golden_helmet)
+        {
+            meta = 1000;
+        }
+        else if (item == Items.diamond_helmet)
+        {
+            meta = 2000;
+        }
+
+        Material material = block.getMaterial();
+        if (material == Material.water)
+        {
+            meta += 1;
+        }
+        else if (material == Material.lava)
+        {
+            meta += 2;
+        }
+        return new ItemStack(HelmBucket.itemHelmBucket, 1, meta);
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
+    {
+        MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(world, player, false);
+
+        if (movingobjectposition == null)
+        {
+            return stack;
+        }
+        else
+        {
+            if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
+            {
+                int i = movingobjectposition.blockX;
+                int j = movingobjectposition.blockY;
+                int k = movingobjectposition.blockZ;
+
+                if (!world.canMineBlock(player, i, j, k))
+                {
+                    return stack;
+                }
+
+                if (movingobjectposition.sideHit == 0)
+                {
+                    --j;
+                }
+
+                if (movingobjectposition.sideHit == 1)
+                {
+                    ++j;
+                }
+
+                if (movingobjectposition.sideHit == 2)
+                {
+                    --k;
+                }
+
+                if (movingobjectposition.sideHit == 3)
+                {
+                    ++k;
+                }
+
+                if (movingobjectposition.sideHit == 4)
+                {
+                    --i;
+                }
+
+                if (movingobjectposition.sideHit == 5)
+                {
+                    ++i;
+                }
+
+                if (!player.canPlayerEdit(i, j, k, movingobjectposition.sideHit, stack))
+                {
+                    return stack;
+                }
+
+                if (this.tryPlaceContainedLiquid(world, i, j, k, stack.getItemDamage() % 1000) && !player.capabilities.isCreativeMode)
+                {
+                    int helm = stack.getItemDamage() / 1000;
+                    switch (helm)
+                    {
+                        case 0:
+                            return new ItemStack(Items.iron_helmet);
+                        case 1:
+                            return new ItemStack(Items.golden_helmet);
+                        case 2:
+                            return new ItemStack(Items.diamond_helmet);
+                    }
+                    return null;
+                }
+            }
+
+            return stack;
+        }
+    }
+
+    /**
+     * Attempts to place the liquid contained inside the bucket.
+     */
+    public boolean tryPlaceContainedLiquid(World world, int x, int y, int z, int fluidMeta)
+    {
+        Material material = world.getBlock(x, y, z).getMaterial();
+        boolean flag = !material.isSolid();
+
+        if (!world.isAirBlock(x, y, z) && !flag)
+        {
+            return false;
+        }
+        else
+        {
+            if (world.provider.isHellWorld && getFluidForMeta(fluidMeta).getMaterial() == Material.water)
+            {
+                world.playSoundEffect((double) ((float) x + 0.5F), (double) ((float) y + 0.5F), (double) ((float) z + 0.5F), "random.fizz", 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+
+                for (int l = 0; l < 8; ++l)
+                {
+                    world.spawnParticle("largesmoke", (double) x + Math.random(), (double) y + Math.random(), (double) z + Math.random(), 0.0D, 0.0D, 0.0D);
+                }
+            }
+            else
+            {
+                if (!world.isRemote && flag && !material.isLiquid())
+                {
+                    world.func_147480_a(x, y, z, true);
+                }
+
+                world.setBlock(x, y, z, getFluidForMeta(fluidMeta), 0, 3);
+            }
+
+            return true;
+        }
+    }
+
+    /**
+     * @param fluidMeta meta % 1000
+     * @return
+     */
+    public Block getFluidForMeta(int fluidMeta)
+    {
+        if (fluidMeta == 1)
+        {
+            return Blocks.water;
+        }
+        else if (fluidMeta == 2)
+        {
+            return Blocks.lava;
+        }
+        return Blocks.air;
     }
 }
